1676f08b260cd35100f3a4e4bf730ca78134723b
Serge Korolchuk <sergey.korolchuk.dev@gmail.com>
2014-03-30 17:14:41 +0300

    Issue #1: Added Account Controller

---
+++ Site/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Site.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
